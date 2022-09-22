659d3d88c31875721c724b5a9810617a9f6d51f0
Thearchy.Helios <jade-topspin.0v@icloud.com>
2022-09-22 15:26:03 +0200

    TP2 First Commit

---
+++ TP2/TP2/test_tri.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "es_tableau.h"
+#include "tri.h"
+
+void test_tri_insertion(int argc, char **argv) { /* À compléter */
+}
+
+void test_tri_insertion_alea(int argc, char **argv) { /* À compléter */
+}
